f640363e3d89b79566b9ca0fc85ea76a2ceab92a
Yunus Emre Ertunç <34343971+MrYunusEmre@users.noreply.github.com>
2022-04-08 11:24:57 +0300

    get_pos_tags added

--- FrequencyMethod.py
+++ FrequencyMethod.py
@@ -1,11 +1,10 @@
 import logging
 import string
 
-from cffi.model import StructType
 from nltk import FreqDist
 from nltk.util import ngrams
 from DatasetHandler import DatasetHandler
-
+from zemberek import TurkishMorphology
 
 class FrequencyMethod:
     def __init__(self, dataset_handler: DatasetHandler, n, morphology_mode):
@@ -24,6 +23,8 @@ class FrequencyMethod:
         self.frequency_ngram_dist = FreqDist()
         self.most_commons = list()
 
+        self.pos_tagger = TurkishMorphology.create_with_defaults()
+
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.info(f'{self.__class__.__name__} object is initialized.')
 
@@ -37,15 +38,13 @@ class FrequencyMethod:
             n_grams = ngrams(self.data, self.n)
 
         self.frequency_ngram_dist.update(n_grams)
-        self.__convert_to_list()
+
+        #self.__convert_to_list()
 
         self.logger.info('Frequency method has done.')
 
         return self.most_commons
 
-    def __convert_to_list(self):
-        for ngram in self.frequency_ngram_dist.most_common(20):
-            self.most_commons.append((f'{ngram[0][0]} {ngram[0][1]}', ngram[1]))
 
     def set_morphology_mode(self, morphology_mode):
         if morphology_mode:
@@ -56,3 +55,22 @@ class FrequencyMethod:
         self.morphology_mode = morphology_mode
 
         self.logger.info(f'morphology_mode is set to {self.morphology_mode}')
+
+
+    def get_pos_tags(self,word):
+        r_tags = []
+        tags = self.pos_tagger.analyze(word.strip())
+        tags_str = str(tags)
+
+        if ":Adj" in tags_str:
+            r_tags.append("A")
+
+        if ":Noun" in tags_str:
+            r_tags.append("N")
+
+        if len(r_tags) > 0:
+            return r_tags[0]
+        return ''
+
+
+
