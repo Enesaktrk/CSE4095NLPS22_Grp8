a4283cf287dce67bd81f6ac8a50d9c31d38ad758
Yunus Emre Ertunç <34343971+MrYunusEmre@users.noreply.github.com>
2022-05-05 16:17:46 +0300

    DatasetCreator is updated.

--- delivery2/DatasetCreator.py
+++ delivery2/DatasetCreator.py
@@ -10,15 +10,14 @@ FOLDER_NAME = "2021-01"
 def read_data():
     # Open folder and read unique .json files
     json_folder_path = os.path.join(FOLDER_NAME)
-    json_files = [int(x[:x.find(".")]) for x in os.listdir(json_folder_path) if
+    json_files = [x for x in os.listdir(json_folder_path) if
                   x.endswith("json") and not (x.__contains__("("))]
 
-    json_data = OrderedDict()
-    sorted_json_files = sorted(json_files)
-    for json_file in sorted_json_files:
-        json_file_path = os.path.join(json_folder_path, str(json_file)+".json")
+    json_data = {}
+    for json_file in json_files:
+        json_file_path = os.path.join(json_folder_path, json_file)
         with open(json_file_path, "r", encoding="utf-8") as f:
-            json_data[str(json_file)] = getText(f)  # {filename : text}
+            json_data[json_file] = getText(f)  # {filename : text}
 
     return json_data
 
